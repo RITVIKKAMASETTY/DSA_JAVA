7fba0a191fb3cf2a12afd68f768ae2b975e3243a
RITVIKKAMASETTY <ritvikksym@gmail.com>
2024-10-10 15:50:40 +0530

    add file

---
+++ a.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class a
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a[]={1,2,3,4,5};
+        int b=a[0];
+        int c=0,d=0;
+        for(int i=1;i<5;i++)
+        {
+            if(b<a[i])
+            {
+                c=a[i]-b;
+                d=Math.max(d,c);
+            }
+            else
+            {
+                b=a[i];
+            }
+        }
+        System.out.println(d);}}
